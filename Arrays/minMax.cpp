2c1dc01263618b3322974621c6d671a8e0dcfeff
redmoon <bsef19a507@pucit.edu.pk>
2022-06-05 07:42:51 +0500

    Maximum and Minimum Element in an Array

--- Arrays/minMax.cpp
+++ Arrays/minMax.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 class Pair{
 	public:
 		int min;
 		int max;
-}
-
-Pair getMinMax(arr[], size){
+	void display(){
+		cout<<"\nMax in Array is : "<<min<<endl;
+		cout<<"Min in Array is : "<<max<<endl;
+	}	
+};
+//getMinMax(arr, size);
+Pair getMinMax(arr, size){
 	Pair minMax;
 	if(size==0){
 		minMax.max = 0;
@@ -30,14 +36,22 @@ Pair getMinMax(arr[], size){
 			minMax.min = arr[i];
 		}
 	}
+	return minMax;
 }
 
 // driver function 
 int main()
 {  
+	Pair minMax;
+	int array_of_numbers = {10,334,32,43,32,35,2,123,12,24}
+	minMax = getMinMax(array_of_numbers, 10);
+	minMax.display();
+	
 
-return 0;  
+	return 0;  
 } 
+
+
 
 
 
