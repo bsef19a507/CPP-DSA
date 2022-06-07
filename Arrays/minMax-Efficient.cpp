61c49951cd3890dfe4030442093ee918f881fabe
redmoon <bsef19a507@pucit.edu.pk>
2022-06-07 22:57:12 +0500

    Maximum and Minimum in Efficient Way

--- Arrays/minMax-Efficient.cpp
+++ Arrays/minMax-Efficient.cpp
@@ -17,62 +17,71 @@ class Pair{
 	}	
 };
 
-
-//getMinMax function that return an object of Pair class containing min ana Max numbers...
-Pair getMinMax(int arr[], int size){
-	Pair minMax;
-	if(size==1){
-		minMax.max = arr[0];
-		minMax.min = arr[0];
-		return minMax;
-	}else if(size==0){
-		minMax.max = 0;
-		minMax.min = 0;
-		return minMax;
-	}
-	 
-	if(arr[0]>arr[1]){
-		minMax.max = arr[0];
-		minMax.min = arr[1];
-	}else{
-		minMax.max = arr[1];
-		minMax.min = arr[0];
-	}
-	
-	for(int counter = 2; counter < size; counter++){
-		if(arr[counter]>minMax.max){
-			minMax.max = arr[counter];
-		}
-		if(arr[counter]<minMax.min){
-			minMax.min = arr[counter];
-		}
-	}
-	return minMax;
+//
+struct Pair getMinMax(int arr[], int low,
+                                 int high)
+{
+    struct Pair minmax, mml, mmr;    
+    int mid;
+     
+    // If there is only one element
+    if (low == high)
+    {
+        minmax.max = arr[low];
+        minmax.min = arr[low];    
+        return minmax;
+    }
+     
+    // If there are two elements
+    if (high == low + 1)
+    {
+        if (arr[low] > arr[high])
+        {
+            minmax.max = arr[low];
+            minmax.min = arr[high];
+        }
+        else
+        {
+            minmax.max = arr[high];
+            minmax.min = arr[low];
+        }
+        return minmax;
+    }
+     
+    // If there are more than 2 elements
+    mid = (low + high) / 2;
+    mml = getMinMax(arr, low, mid);
+    mmr = getMinMax(arr, mid + 1, high);
+     
+    // Compare minimums of two parts
+    if (mml.min < mmr.min)
+        minmax.min = mml.min;
+    else
+        minmax.min = mmr.min;    
+     
+    // Compare maximums of two parts
+    if (mml.max > mmr.max)
+        minmax.max = mml.max;
+    else
+        minmax.max = mmr.max;    
+     
+    return minmax;
 }
-
-// driver function 
+ 
+// Driver code
 int main()
-{  
-	Pair minMax;
-	int array_of_numbers[10] = {10,334,32,43,32,35,2,123,12,24};
-	minMax = getMinMax(array_of_numbers, 10);
-	cout<<"Array : {";
-	for(int i = 0 ; i<10;i++){
-		if(i<9){
-			cout<<array_of_numbers[i]<<", ";
-		}else{
-			cout<<array_of_numbers[i];
-		}
-	
-	}
-	cout<<"}"<<endl;
-	minMax.display();
-
-	return 0;  
-} 
-
-
-
-
-
-
+{
+    int arr[] = { 1000, 11, 445,
+                  1, 330, 3000 };
+    int arr_size = 6;
+     
+    struct Pair minmax = getMinMax(arr, 0,
+                             arr_size - 1);
+                              
+    cout << "Minimum element is "
+         << minmax.min << endl;
+    cout << "Maximum element is "
+         << minmax.max;
+          
+    return 0;
+}
