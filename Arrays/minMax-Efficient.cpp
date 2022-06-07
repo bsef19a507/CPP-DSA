dc9d2386003151bcaf216d0139e4bd021e8accfb
redmoon <bsef19a507@pucit.edu.pk>
2022-06-07 22:52:59 +0500

    Maximum and Minimum in Efficient Way

---
+++ Arrays/minMax-Efficient.cpp
@@ -0,0 +1,78 @@
+/*******************************************
+Maximum and minimum of an Array
+*******************************************/
+
+#include <iostream>
+
+using namespace std;
+
+// class Pair to get min and max number to return from functin...
+class Pair{
+	public:
+		int min;
+		int max;
+	void display(){
+		cout<<"\nMax in Array is : "<<min<<endl;
+		cout<<"Min in Array is : "<<max<<endl;
+	}	
+};
+
+
+//getMinMax function that return an object of Pair class containing min ana Max numbers...
+Pair getMinMax(int arr[], int size){
+	Pair minMax;
+	if(size==1){
+		minMax.max = arr[0];
+		minMax.min = arr[0];
+		return minMax;
+	}else if(size==0){
+		minMax.max = 0;
+		minMax.min = 0;
+		return minMax;
+	}
+	 
+	if(arr[0]>arr[1]){
+		minMax.max = arr[0];
+		minMax.min = arr[1];
+	}else{
+		minMax.max = arr[1];
+		minMax.min = arr[0];
+	}
+	
+	for(int counter = 2; counter < size; counter++){
+		if(arr[counter]>minMax.max){
+			minMax.max = arr[counter];
+		}
+		if(arr[counter]<minMax.min){
+			minMax.min = arr[counter];
+		}
+	}
+	return minMax;
+}
+
+// driver function 
+int main()
+{  
+	Pair minMax;
+	int array_of_numbers[10] = {10,334,32,43,32,35,2,123,12,24};
+	minMax = getMinMax(array_of_numbers, 10);
+	cout<<"Array : {";
+	for(int i = 0 ; i<10;i++){
+		if(i<9){
+			cout<<array_of_numbers[i]<<", ";
+		}else{
+			cout<<array_of_numbers[i];
+		}
+	
+	}
+	cout<<"}"<<endl;
+	minMax.display();
+
+	return 0;  
+} 
+
+
+
+
+
+
